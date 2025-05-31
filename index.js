03054000ced6d53b2a75d716d6855e5de204e656
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-05-31 19:22:06 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const {
+  H,
+  sqrt_mod,
+  T,
+  _pow_T_range,
+  keygen,
+  encrypt,
+  decrypt,
+} = require('./src/lai');
+
+module.exports = {
+  H,
+  sqrt_mod,
+  T,
+  _pow_T_range,
+  keygen,
+  encrypt,
+  decrypt,
+};
