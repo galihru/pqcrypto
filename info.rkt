4c7cc726c3d8230f760c10fef7a28e2f95982f1b
GALIH RIDHO UTOMO <92840324+galihru@users.noreply.github.com>
2025-07-05 07:05:05 +0700

    Create info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "laicrypto")
+(define deps '("base" "crypto-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "rackunit-lib"))
+(define scribblings '(("scribblings/pqcrypto-lai.scrbl" ())))
+(define pkg-desc "Lemniscate-AGM Isogeny (LAI) Encryption")
+(define version "0.1")
+(define pkg-authors '("GALIH RIDHO UTOMO"))
+(define license 'MIT)
